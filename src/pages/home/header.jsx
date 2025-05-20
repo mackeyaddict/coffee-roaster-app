eac5cd13d4247dbbe743feb3cf243ce715b64dd4
mackeyaddict <naufalyusuf90@gmail.com>
2025-05-20 10:25:13 +0700

    feat: change language to indonesian

--- src/pages/home/header.jsx
+++ src/pages/home/header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
       <div className="relative flex flex-col items-center justify-center text-white z-10 text-center px-4">
         {/* <Image src={Logo} alt="Logo" width={200} height={200} preview={false} /> */}
         <h1 className="text-4xl md:text-5xl font-bold mb-4">Homie Home Roaster</h1>
-        <p className="text-xl md:text-2xl max-w-2xl">Crafting the perfect roast for exceptional coffee experiences</p>
+        <p className="text-xl md:text-2xl max-w-2xl">Pemanggangan yang sempurna untuk pengalaman kopi yang luar biasa</p>
       </div>
     </header>
   );
