fc4133b9c3e2329420232d0ff98cd8b47072cb5e
mackeyaddict <naufalyusuf90@gmail.com>
2025-05-20 10:38:54 +0700

    feat: change langugage to indonesian

--- src/pages/home/feature.jsx
+++ src/pages/home/feature.jsx
@@ -1,23 +1,23 @@
-import { ChartSpline, Timer, ToggleRight,  } from 'lucide-react';
+import { ChartSpline, Timer, ToggleRight, } from 'lucide-react';
 import FeatureCard from '../../components/featureCard';
 export default function Feature() {
   const features = [
     {
       icon: ChartSpline,
       title: "Temperature Monitor",
-      description: "Track and visualize precise temperature curves throughout the roasting process for consistent results every time.",
+      description: "Pantau dan visualisasikan kurva suhu secara presisi selama proses pemanggangan untuk memastikan hasil yang konsisten setiap saat.",
       iconColor: "text-red-500"
     },
     {
       icon: Timer,
       title: "Timer",
-      description: "Set precise timing intervals for each roast phase with customizable alerts to achieve your perfect roast profile.",
+      description: "Atur interval waktu yang tepat untuk setiap fase pemanggangan dengan notifikasi yang dapat disesuaikan guna mencapai profil roasting yang sempurna.",
       iconColor: "text-green-500"
     },
     {
       icon: ToggleRight,
       title: "Full Control",
-      description: "Adjust heat, airflow, and drum speed in real-time with intuitive controls designed for professional roasters.",
+      description: "Sesuaikan panas dan rotator secara real-time melalui kontrol intuitif yang dirancang untuk roaster profesional.",
       iconColor: "text-blue-500"
     }
   ];
@@ -26,12 +26,12 @@ export default function Feature() {
     <section className="py-16 bg-gray-100">
       <div className="container mx-auto px-4">
         <div className="text-center mb-12">
-          <h2 className="text-3xl font-bold text-gray-900 mb-4">Features</h2>
+          <h2 className="text-3xl font-bold text-gray-900 mb-4">Fitur-Fitur</h2>
           <p className="text-xl text-gray-600 max-w-2xl mx-auto">
-            Discover how our solution can transform your workflow with these powerful features
+            Temukan bagaimana solusi kami dapat menyempurnakan proses roasting kopi Anda dengan fitur-fitur canggih berikut ini.
           </p>
         </div>
-        
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 border border-gray-300 p-8 rounded-2xl">
           {features.map((feature, index) => (
             <FeatureCard
